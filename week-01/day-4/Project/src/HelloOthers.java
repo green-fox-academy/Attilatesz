ce9068436bb226832bfad11ba6f12bf88b7f217d
Attilatesz <attila.sloszar@gmail.com>
2018-02-15 12:38:32 +0100

    First Java Project

---
+++ week-01/day-4/Project/src/HelloOthers.java
@@ -0,0 +1,9 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        //
+        System.out.println("Hello, Gyuri!");
+        System.out.println("Hello, Greg!");
+        System.out.println("Hello, Dominik!");
+    }
+}
