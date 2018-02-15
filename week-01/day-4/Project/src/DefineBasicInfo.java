4622bf4b7a7039a181170550213d07df26b60a5a
Attilatesz <attila.sloszar@gmail.com>
2018-02-15 15:30:18 +0100

    First Java Project

---
+++ week-01/day-4/Project/src/DefineBasicInfo.java
@@ -0,0 +1,22 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+
+        String Name = "Attila Sloszar";
+        int Age = 27;
+        double Height = 1.95;
+        boolean married = false;
+        boolean notMarried = true;
+
+        System.out.println("My name is " + Name + " .");
+        System.out.println("I'm " + Age + " old.");
+        System.out.println("My height is " + Height + " m.");
+        if (notMarried = true){
+            System.out.println("Not married!");
+        }
+    }
+}
