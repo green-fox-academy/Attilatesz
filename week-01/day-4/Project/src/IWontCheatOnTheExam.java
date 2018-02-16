ccbcf124004f2d286cfe4bdf8a1ba50d562321b9
Attilatesz <attila.sloszar@gmail.com>
2018-02-16 11:09:10 +0100

    Multiplication

---
+++ week-01/day-4/Project/src/IWontCheatOnTheExam.java
@@ -0,0 +1,11 @@
+public class IWontCheatOnTheExam {
+    public static void main(String[] args){
+        String exam = "I won't cheat on the exam";
+        for (int a = 0; a < 100; a++){
+            System.out.print(exam );
+        }
+
+    }
+}
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
