c4cf373a75cca8ac921a428ee0644e95b425920e
Attilatesz <attila.sloszar@gmail.com>
2018-02-19 12:58:52 +0100

    Functions part1

---
+++ week-02/day-01/src/Summing.java
@@ -0,0 +1,11 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter and returns with an integer
+
+
+public class Summing {
+    public static void main(String[] args) {
+        }
+}
