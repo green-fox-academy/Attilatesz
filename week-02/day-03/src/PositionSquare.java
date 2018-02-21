988520110688b7733c1d8f3502bf1e782eeca589
Attilatesz <attila.sloszar@gmail.com>
2018-02-21 12:48:18 +0100

    3 random squares

--- week-02/day-03/src/PositionSquare.java
+++ week-02/day-03/src/PositionSquare.java
@@ -5,28 +5,40 @@ import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FourRetangles {
+public class PositionSquare {
 
     public static void mainDraw(Graphics graphics) {
-        // draw four different size and color rectangles.
+        // create a square drawing function that takes 2 parameters:
+        // the x and y coordinates of the square's top left corner
+        // and draws a 50x50 square from that point.
+        // draw 3 squares with that function.
         // avoid code duplication.
 
+        int x = 0;
+        int y = 0;
 
-        for (int i = 0; i < 4; i++) {
-            Random random = new Random();
-            int x = random.nextInt(321);
-            int y = random.nextInt(361);
-            int width = random.nextInt(100);
-            int height = random.nextInt(100);
-            graphics.fillRect(x, y, width, height);
-            graphics.setColor(Color.getHSBColor(random.nextInt(361), random.nextInt(100) + 80, 100));
-        }
+        drawSquare(x,y,graphics);
+
 
     }
+        public static void drawSquare(int width, int heigth, Graphics square) {
+            Random random = new Random();
+
+            for (int i = 0; i < 3; i++) {
+                width = random.nextInt(271);
+                heigth = random.nextInt(311);
+                square.drawRect(width, heigth, 50, 50);
+
+
+            }
+        }
+
+
+
 
     //    Don't touch the code below
     static int WIDTH = 320;
-    static int HEIGHT = 360;
+    static int HEIGHT = 343;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
@@ -36,8 +48,7 @@ public class FourRetangles {
         jFrame.setLocationRelativeTo(null);
         jFrame.setVisible(true);
     }
-
-    static class ImagePanel extends JPanel {
+    static class ImagePanel extends JPanel{
         @Override
         protected void paintComponent(Graphics graphics) {
             super.paintComponent(graphics);
