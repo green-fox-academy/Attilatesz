87af1926a0dbc7fc131f4c03f4bf952c760e21d9
Attilatesz <attila.sloszar@gmail.com>
2018-02-21 10:52:26 +0100

    ColorBox

---
+++ week-02/day-03/src/ColoredBox.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+
+        graphics.setColor(Color.BLUE);
+        graphics.drawLine(40, 40, 100,40);
+        graphics.setColor(Color.RED);
+        graphics.drawLine(100,40,100,80);
+        graphics.setColor(Color.YELLOW);
+        graphics.drawLine(100,80,40,80);
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(40,80,40,40);
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 360;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
