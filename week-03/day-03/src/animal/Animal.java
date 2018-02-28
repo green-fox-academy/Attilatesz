bbb6c61b6423d307aca568bedab3294518af1f36
Attilatesz <attila.sloszar@gmail.com>
2018-02-28 15:15:21 +0100

    Animals

---
+++ week-03/day-03/src/animal/Animal.java
@@ -0,0 +1,38 @@
+package animal;
+
+public class Animal {
+
+  public int hunger = 50;
+  public int thurst = 50;
+
+
+  public Animal(){
+
+    this.hunger = hunger;
+    this.thurst = thurst;
+
+  }
+
+  @Override
+  public String toString() {
+    return "Animal{" +
+            "hunger=" + hunger +
+            ", thurst=" + thurst +
+            '}';
+  }
+
+  public void eat(){
+    hunger--;
+  }
+
+  public void drink(){
+    thurst--;
+  }
+
+  public void play(){
+    hunger--;
+    thurst--;
+  }
+
+}
+
---
+++ week-03/day-03/src/animal/AnimalTry.java
@@ -0,0 +1,15 @@
+package animal;
+
+public class AnimalTry{
+
+public static void main(String[]args) {
+
+  Animal cat = new Animal();
+  cat.eat();
+  cat.play();
+  cat.drink();
+
+  System.out.println(cat.toString());
+  }
+
+}
