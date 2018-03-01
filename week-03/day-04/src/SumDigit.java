482925fa55927198776f68b5d27215a685e876fe
Attilatesz <attila.sloszar@gmail.com>
2018-03-01 10:36:35 +0100

    sumdigit

--- week-03/day-04/src/SumDigit.java
+++ week-03/day-04/src/SumDigit.java
@@ -1,3 +1,13 @@
 public class SumDigit {
-  
+  public static void main(String[] args) {
+    System.out.println(summer(587));
+  }
+
+  private static int summer(int n) {
+    if (n == 0) {
+      return 0;
+    } else{
+      return (n % 10)+summer(n/10);
+    }
+  }
 }
