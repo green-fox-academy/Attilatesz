8b35938f62992d8d22b1f93266f885364ed92aaa
Attilatesz <attila.sloszar@gmail.com>
2018-03-01 13:03:41 +0100

    Factorial 2.0

--- week-03/day-04/src/Refactorio.java
+++ week-03/day-04/src/Refactorio.java
@@ -1,13 +1,14 @@
 public class Refactorio {
   public static void main(String[] args) {
-    System.out.println(factorial(2));
+
+    System.out.println(factorial(5));
   }
 
   private static int factorial(int n) {
     if (n == 1){
       return 1;
     }else {
-      return n * factorial(factorial(n-1));
+      return n * factorial(n-1);
     }
   }
 }
