5b4d48807f843ec7bab43d94010cd330233c54c1
Attilatesz <attila.sloszar@gmail.com>
2018-03-02 13:54:28 +0100

    finalfinalsquare

--- week-03/day-04/src/square.java
+++ week-03/day-04/src/square.java
@@ -1,4 +1,3 @@
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,51 +7,46 @@ public class square {
 
   public static void mainDraw(Graphics graphics) {
 
-
-
-    drawSquare(5,100,100,200,20, graphics);
+    drawSquare(8,150,150,300,30, graphics);
 
   }
-
 
   private static void drawSquare(int level, int x, int y, int squareSize, int lineWidth, Graphics graphics) {
     if (level == 1) {
       return;
-
     } else {
 
-      Color newColorOne = new Color(0,255,255);
-      Color newColorTwo = new Color(255,255,255);
+      Color newColorOne = new Color(255,255,255);
+      Color newColorTwo = new Color(90,90,90);
 
       graphics.setColor(newColorOne);
       graphics.fillRect(x, y, squareSize, lineWidth);
 
       graphics.setColor(newColorTwo);
-      graphics.fillRect(x+lineWidth, y+squareSize, squareSize, lineWidth);
+      graphics.fillRect(x + lineWidth, y + squareSize, squareSize, lineWidth);
 
       graphics.setColor(newColorOne);
-      graphics.fillRect(x, y, lineWidth, lineWidth+squareSize);
+      graphics.fillRect(x, y, lineWidth, lineWidth + squareSize);
 
       graphics.setColor(newColorTwo);
-      graphics.fillRect(x+squareSize, y, lineWidth, squareSize);
+      graphics.fillRect(x + squareSize, y, lineWidth, squareSize);
 
-      int newSquareSize = squareSize/2;
-      int newLineWidth = lineWidth/2;
-      int x1 = x-newSquareSize/2;
-      int y1 = y-newSquareSize/2;
+      int newSquareSize = squareSize / 2;
+      int newLineWidth = lineWidth / 2;
+      int x1 = x-newSquareSize / 2;
+      int y1 = y-newSquareSize / 2;
 
 
       drawSquare(level-1, x1, y1, newSquareSize, newLineWidth, graphics);
-      drawSquare(level-1, x1+squareSize, y1, newSquareSize, newLineWidth, graphics);
-      drawSquare(level-1, x1, y1+squareSize, newSquareSize, newLineWidth, graphics);
-      drawSquare(level-1, x1+squareSize, y1+squareSize, newSquareSize, newLineWidth ,graphics);
-
-    }
+      drawSquare(level-1, x1 + squareSize, y1, newSquareSize, newLineWidth, graphics);
+      drawSquare(level-1, x1, y1 + squareSize, newSquareSize, newLineWidth, graphics);
+      drawSquare(level-1, x1 + squareSize, y1 + squareSize, newSquareSize, newLineWidth ,graphics);
+      }
   }
 
   //    Don't touch the code below
-  static int WIDTH = 400;
-  static int HEIGHT = 400;
+  static int WIDTH = 600;
+  static int HEIGHT = 600;
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
@@ -61,6 +55,7 @@ public class square {
     jFrame.add(new ImagePanel());
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
+
   }
 static class ImagePanel extends JPanel{
   @Override
