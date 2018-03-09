7e0d0e9c9b403f3e96974ce4702420c7d4ac0c9a
Attilatesz <attila.sloszar@gmail.com>
2018-03-09 10:17:11 +0100

    firstLines

---
+++ week-04/day-05/src/com/company/ToDoUsage.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class ToDoUsage {
+
+  public void todolist(){
+
+    System.out.println("Command Line Todo application");
+    System.out.println("=============================\n");
+    System.out.println("Command line arguments:");
+    System.out.println("-l   Lists all the tasks");
+    System.out.println("-a   Adds a new task");
+    System.out.println("-r   Removes an task");
+    System.out.println("-c   Completes an task");
+  }
+}
