e1879d12c2607382277d8b15c57cbc8bbc0c2e85
Attilatesz <attila.sloszar@gmail.com>
2018-04-03 12:14:23 +0200

    springStart

---
+++ week-007/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,14 @@
+package com.greenfoxacademy.springstart.controllers;
+
+public class Greeting {
+  private long id;
+  private String content;
+
+  public long getId() {
+    return id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
